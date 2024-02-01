9e968de09b29a7d880a80ed30b77d4872c06a060
didcksgh <chanho.yang1226@gmail.com>
2024-01-31 19:52:36 -0600

    store cart activity

---
+++ store_cart/src/Categories.js
@@ -0,0 +1,5 @@
+export const Categories = [
+    "kitchenware",
+    "sports"
+
+];
---
+++ store_cart/src/Products.js
@@ -0,0 +1,111 @@
+export const Products=[
+    {
+        "id" : 1,
+        "title" : "Pot",
+        "price" : 6.99,
+        "description" : "100% stainless. Light and easy to wash.",
+        "category" : "kitchenware",
+        "image" : require("./Images/pot.jpg"),
+        "rating" : {"rate": 3.9}
+    },
+    {
+        "id" : 2,
+        "title" : "Fry pan",
+        "price" : 10.99,
+        "description" : "Non-stick, 100% iron. ",
+        "category" : "kitchenware",
+        "image" : require("./Images/fryiing pan.jpg"),
+        "rating" : {"rate" : 4.8}
+    },
+    {
+        "id" : 3,
+        "title" : "Kettle",
+        "price" : 9.99,
+        "description" : "Easy to wash. Light.",
+        "category" : "kitchenware",
+        "image" : require("./Images/kettle.jpg"),
+        "rating" : {"rate" : 4.1}
+    },
+    {
+        "id" : 4,
+        "title" : "Mug",
+        "price" : 12.99,
+        "description" : "Microwavable. 100% ceramic.",
+        "category" : "kitchenware",
+        "image" :  require("./Images/mug.jpg"),
+        "rating" : {"rate" : 4.7}
+    },
+    {
+        "id" : 5,
+        "title" : "Plate",
+        "price" : 15.99,
+        "description" : "Made of bamboo. Light and eco-friendly plate.",
+        "category" : "kitchenware",
+        "image" :  require("./Images/plate.jpg"),
+        "rating" : {"rate" : 3.6}
+    },
+    {
+        "id" : 6,
+        "title" : "Silverware",
+        "price" : 2.99,
+        "description" : "Siverware sets. Including knife, spoon, and fork.",
+        "category" : "kitchenware",
+        "image" : require("./Images/silverware.jpg"),
+        "rating" : {"rate" : 3.1}
+    },
+    {
+        "id" : 7,
+        "title" : "Soccer cleats",
+        "price" : 40.99,
+        "description" : "New techonology, light and strong. Enhence your speed with this cleats.",
+        "category" : "sports",
+        "image" :  require("./Images/soccer cleats.jpg"),
+        "rating" : {"rate" : 4.9}
+    },
+    {
+        "id" : 8,
+        "title" : "Soccer ball",
+        "price" : 20.99,
+        "description" : "Official ball. Advanced techonology is applied.",
+        "category" : "sports",
+        "image" :  require("./Images/soccer ball.jpg"),
+        "rating" : {"rate" : 3.1}
+    },
+    {
+        "id" : 9,
+        "title" : "Bike",
+        "price" : 40.99,
+        "description" : "made of carbon. Enjoy the light and the speed.",
+        "category" : "sports",
+        "image" :  require("./Images/bike.jpg"),
+        "rating" : {"rate" : 3.8}
+    },
+    {
+        "id" : 10,
+        "title" : "Basketball",
+        "price" : 12.99,
+        "description" : "Official ball.",
+        "category" : "sports",
+        "image" :  require("./Images/basketball.jpg"),
+        "rating" : {"rate" : 4.1}
+    },
+    {
+        "id" : 11,
+        "title" : "Baseball gloves",
+        "price" : 8.99,
+        "description" : "100% leather.",
+        "category" : "sports",
+        "image" :  require("./Images/baseball gloves.jpg"),
+        "rating" : {"rate" : 4.5}
+    },
+    {
+        "id" : 12,
+        "title" : "Football",
+        "price" : 20.99,
+        "description" : "Official ball.",
+        "category" : "sports",
+        "image" :  require("./Images/american football.jpg"),
+        "rating" : {"rate" : 3.6}
+    }
+    
+]
